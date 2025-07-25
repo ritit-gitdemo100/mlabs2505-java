4f24a292b563603fb166b039d45b66b362a0e5f0
Global Developer <ritit.programs@gmail.com>
2025-07-25 18:43:32 +0530

    GitKube

---
+++ SBKubeDemo/src/main/java/com/rit/SbKubeDemoApplication.java
@@ -0,0 +1,13 @@
+package com.rit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbKubeDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbKubeDemoApplication.class, args);
+	}
+
+}
