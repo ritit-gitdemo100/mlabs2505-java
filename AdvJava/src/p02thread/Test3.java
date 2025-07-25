985cb6b3b5f4ddeb4314f478f5451d60fc99b159
Global Developer <ritit.programs@gmail.com>
2025-07-25 18:34:53 +0530

    JenkinsGit

---
+++ AdvJava/src/p02thread/Test3.java
@@ -0,0 +1,5 @@
+class Test3{
+	public static void main(String[] ar){
+		System.out.println("Welcome to Java Jenkins");
+	}
+}
---
+++ GitTest4.java
@@ -0,0 +1,5 @@
+class GitTest4{
+	public static void main(String[] ar){
+		System.out.println("Welcome to Java Jenkins");
+	}
+}
