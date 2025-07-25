8b651c4919e55e915380f1cbd03b02531551cef2
Global Developer <ritit.programs@gmail.com>
2025-07-25 23:15:18 +0530

    Jenkins Java

--- GitTest4.java
+++
@@ -1,5 +0,0 @@
-class GitTest4{
-	public static void main(String[] ar){
-		System.out.println("Welcome to Java Jenkins 123");
-	}
-}
---
+++ JenkinsJavaGit.java
@@ -0,0 +1,6 @@
+class JenkinsJavaGit {
+	public static void main(String[] ar){
+		System.out.println("Welcome to Jenkins Java Build");
+	}
+}
+	
